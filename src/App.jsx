8b033ce772e7d2e9df6a2695c22a519076536fd8
Nitish Singh <singhnitish198@gmail.com>
2025-04-12 15:42:32 +0530

    feat(Hero): Hero Section is almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Hero from "./sections/Hero";
+
 const App = () => {
 	return (
-		<div className="text-2xl text-amber-200">Hi Nitish! Let's get going</div>
+		<div>
+			<Hero />
+		</div>
 	);
 };
 
