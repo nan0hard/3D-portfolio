693c5cd9cab33e4f995086330c8f45269b0f9937
Nitish Singh <singhnitish198@gmail.com>
2025-04-16 00:57:46 +0530

    feat(card): Feature Card + Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import FeatureCards from "./sections/FeatureCards";
 import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import Showcase from "./sections/Showcase";
+import Experience from "./sections/Experience";
 
 const App = () => {
 	return (
@@ -8,6 +10,8 @@ const App = () => {
 			<Navbar />
 			<Hero />
 			<Showcase />
+			<FeatureCards />
+			<Experience />
 		</div>
 	);
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -22,51 +22,11 @@ const words = [
 	{ text: "Concepts", imgPath: "/images/concepts.svg" },
 	{ text: "Designs", imgPath: "/images/designs.svg" },
 	{ text: "Code", imgPath: "/images/code.svg" },
-	{ text: "Ideas", imgPath: "/images/ideas.svg" },
-	{ text: "Concepts", imgPath: "/images/concepts.svg" },
-	{ text: "Designs", imgPath: "/images/designs.svg" },
-	{ text: "Code", imgPath: "/images/code.svg" },
 ];
 
 const counterItems = [
 	{ value: 3, suffix: "+", label: "Years of Experience" },
 	{ value: 10, suffix: "+", label: "Completed Projects" },
-];
-
-const logoIconsList = [
-	{
-		imgPath: "/images/logos/company-logo-1.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-2.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-3.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-4.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-5.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-6.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-7.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-8.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-9.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-10.png",
-	},
-	{
-		imgPath: "/images/logos/company-logo-11.png",
-	},
 ];
 
 const abilities = [
@@ -77,8 +37,8 @@ const abilities = [
 	},
 	{
 		imgPath: "/images/chat.png",
-		title: "Reliable Communication",
-		desc: "Keeping you updated at every step to ensure transparency and clarity.",
+		title: "Collaboration & Communication",
+		desc: "Skilled communicator with a proven ability to keep both technical and non-technical stakeholders informed and engaged",
 	},
 	{
 		imgPath: "/images/time.png",
@@ -263,7 +223,6 @@ const socialImgs = [
 export {
 	words,
 	abilities,
-	logoIconsList,
 	counterItems,
 	expCards,
 	expLogos,
