528f1aac56ff7a5886222f4bd22cd23510b6b0ab
Nitish Singh <singhnitish198@gmail.com>
2025-04-15 23:35:32 +0530

    feat(navbar): navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import Showcase from "./sections/Showcase";
 
 const App = () => {
 	return (
 		<div>
+			<Navbar />
 			<Hero />
 			<Showcase />
 		</div>
